5209ea1a233b9c35a4c60ec1141f4fee284d05c0
Streamline <84669200+Streamline6@users.noreply.github.com>
2022-04-04 11:09:01 +0530

    Update TestModals.js

--- components/TestModals.js
+++ components/TestModals.js
@@ -74,39 +74,3 @@ const TestModals = () => {
 
 export default TestModals;
 
-// {showModal ? (
-//     <>
-//       <div className="fadeIn flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-//         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-//           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-//             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-//               <h3 className="text-3xl font=semibold">General Info</h3>
-//               <button
-//                 className="bg-transparent border-0 text-black float-right"
-//                 onClick={() => setShowModal(false)}
-//               >
-//                 <HiX />
-//               </button>
-//             </div>
-
-//             <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-//               <button
-//                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-//                 type="button"
-//                 onClick={() => setShowModal(false)}
-//               >
-//                 Close
-//               </button>
-//               <button
-//                 className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-//                 type="button"
-//                 onClick={() => setShowModal(false)}
-//               >
-//                 Submit
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   ) : null}
